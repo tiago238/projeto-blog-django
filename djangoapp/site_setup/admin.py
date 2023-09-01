b3604de4c251e76b01975a0d4005c86e0cf0fbe6
Tiago Martins <tiago_martins238@hotmail.com>
2023-09-01 19:12:15 -0300

    create favicon model field

--- djangoapp/site_setup/admin.py
+++ djangoapp/site_setup/admin.py
@@ -1,6 +1,21 @@
 from django.contrib import admin
-from site_setup.models import MenuLink
+from django.http.request import HttpRequest
+from site_setup.models import MenuLink, SiteSetup
 
-@admin.register(MenuLink)
-class MenuLinkAdmin(admin.ModelAdmin):
-    list_display = 'text', 'url_or_path',
+# @admin.register(MenuLink)
+# class MenuLinkAdmin(admin.ModelAdmin):
+#     list_display = 'id', 'text', 'url_or_path',
+#     list_display_links = 'id', 'text', 'url_or_path',
+#     search_fields = 'id', 'text', 'url_or_path',
+
+class MenuLinkInLine(admin.TabularInline):
+    model = MenuLink    
+
+@admin.register(SiteSetup)
+class SiteSetupAdmin(admin.ModelAdmin):
+    list_display = 'title', 'description',
+    inlines = MenuLinkInLine,
+
+    def has_add_permission(self, request: HttpRequest) -> bool:
+        return not SiteSetup.objects.exists()
+   
