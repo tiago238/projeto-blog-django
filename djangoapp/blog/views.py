afa68af73d0330c69275d00ba1821d688dc19453
Tiago Martins <tiago_martins238@hotmail.com>
2023-09-02 09:50:32 -0300

    add css and html into pages and partials

--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -1,10 +1,43 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
 
+posts = list(range(1000))
+
 def index(request):
-    return render(request,'blog/pages/index.html')
+    paginator = Paginator(posts, 9)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    return render(
+        request,
+        'blog/pages/index.html',
+        {
+            'page_obj': page_obj,
+        }
+    )
 
 def page(request):
-    return render(request,'blog/pages/page.html')
+    paginator = Paginator(posts, 9)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    return render(
+        request,
+        'blog/pages/page.html',
+        {
+            # 'page_obj': page_obj,
+        }
+    )
 
 def post(request):
-    return render(request,'blog/pages/post.html')
+    paginator = Paginator(posts, 9)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    return render(
+        request,
+        'blog/pages/post.html',
+        {
+            # 'page_obj': page_obj,
+        }
+    )
