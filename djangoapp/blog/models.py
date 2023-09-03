99ee190d7653a796523012b4b5456a646f8e9427
Tiago Martins <tiago_martins238@hotmail.com>
2023-09-03 15:11:59 -0300

    created models page and tag

--- djangoapp/blog/models.py
+++ djangoapp/blog/models.py
@@ -16,6 +16,9 @@ class Tag(models.Model):
         if not self.slug:
             self.slug = slugify_new(self.name, 4)
         return super().save(*args, **kwargs)
+    
+    def __str__(self) -> str:
+        return self.name
 
 class Category(models.Model):
     class Meta:
@@ -31,3 +34,33 @@ class Category(models.Model):
         if not self.slug:
             self.slug = slugify_new(self.name, 4)
         return super().save(*args, **kwargs)
+
+    def __str__(self) -> str:
+        return self.name
+    
+class Page(models.Model):
+    class Meta:
+        verbose_name = 'Page'
+        verbose_name_plural = 'Pages'
+    title = models.CharField(max_length=65)
+    slug = models.SlugField(
+        unique=True, default="",
+        null=False, blank=True, max_length=255,
+    )
+
+    is_published = models.BooleanField(
+        default=False,
+        help_text=(
+            'Este campo precisará estar marcado '
+            'para a página ser exibida publicamente.'
+        ),
+    )
+    content = models.TextField(default="")
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify_new(self.title, 4)
+        return super().save(*args, **kwargs)
+    
+    def __str__(self) -> str:
+        return self.title
