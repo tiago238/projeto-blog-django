b7dbeec42eb2a45106c54e6d036ebb3fcb31d873
Tiago Martins <tiago_martins238@hotmail.com>
2023-09-03 23:44:41 -0300

    created post model

--- djangoapp/blog/models.py
+++ djangoapp/blog/models.py
@@ -1,6 +1,6 @@
-from typing import Iterable, Optional
 from django.db import models
 from utils.rands import slugify_new 
+from django.contrib.auth.models import User
 
 class Tag(models.Model):
     class Meta:
@@ -56,6 +56,57 @@ class Page(models.Model):
         ),
     )
     content = models.TextField(default="")
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify_new(self.title, 4)
+        return super().save(*args, **kwargs)
+    
+    def __str__(self) -> str:
+        return self.title
+    
+class Post(models.Model):
+    class Meta:
+        verbose_name = 'Post'
+        verbose_name_plural = 'Posts'
+    title = models.CharField(max_length=65)
+    slug = models.SlugField(
+        unique=True, default="",
+        null=False, blank=True, max_length=255,
+    )
+    excerpt = models.CharField(max_length=150)
+    is_published = models.BooleanField(
+        default=False,
+        help_text=(
+            'Este campo precisará estar marcado '
+            'para a página ser exibida publicamente.'
+        ),
+    )
+    content = models.TextField(default="")
+    cover = models.ImageField(upload_to='posts/%Y/%m/', blank=True, default='')
+    cover_in_post_content = models.BooleanField(
+        default=True,
+        help_text='Se marcado, exibirá a capa dentro do post.',
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        blank=True, null=True,
+        related_name='page_created_by'
+    )
+    updated_at = models.DateTimeField(auto_now=True)
+    updated_by=models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL, 
+        blank=True, null=True,
+        related_name = 'post_updated_by'
+    )
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True, blank=True, 
+        default=None,
+    )
+    tags = models.ManyToManyField(Tag, blank=True,default='')
 
     def save(self, *args, **kwargs):
         if not self.slug:
