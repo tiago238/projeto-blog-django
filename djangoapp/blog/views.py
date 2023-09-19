3d4514537096743ad67fa06598f4e6ee71a8ec9b
Tiago Martins <tiago_martins238@hotmail.com>
2023-09-18 23:13:59 -0300

    created class CreatedByListView

--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -1,11 +1,12 @@
 from typing import Any, Dict
+from django import http
 from django.core.paginator import Paginator
 from django.db.models.query import QuerySet
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from blog.models import Post, Page
 from django.db.models import Q
 from django.contrib.auth.models import User
-from django.http import Http404
+from django.http import Http404, HttpRequest, HttpResponse
 from django.views.generic import ListView
 
 PER_PAGE = 9
@@ -53,8 +54,44 @@ def created_by(request, author_pk):
     )
 
 class CreatedByListView(PostListView):
-    ...
 
+    def __init__(self, **kwargs: Any) -> None:
+        super().__init__(**kwargs)
+        self._temp_context: dict[str, Any] = {}
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        user = self._temp_context['user']
+        user_full_name = user.username
+
+        if user.first_name:
+            user_full_name = f'{user.first_name} {user.last_name}'
+        page_title = 'Posts de ' + user_full_name + ' - '
+
+        ctx.update({
+            'page_title': page_title,
+        })
+
+        return ctx
+
+    def get_queryset(self) -> QuerySet[Any]:
+        qs = super().get_queryset()
+        qs = qs.filter(created_by__pk=self._temp_context['user'].pk)
+        return  qs
+
+    def get(self, request: HttpRequest, *args: Any, **kwargs: Any) -> HttpResponse:
+            author_pk = self.kwargs.get('author_pk')
+            user = User.objects.filter(pk=author_pk).first()
+
+            if user is None:
+                raise Http404()
+            self._temp_context.update({
+                'author_pk': author_pk,
+                'user': user,
+            })
+
+            return super().get(request, *args, **kwargs)
+    
 def category(request, slug):
     posts = (
         Post.objManager.get_published()
